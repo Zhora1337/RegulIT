5969f38ba547f2bd40873ad4bfc2500b8925f1a8
Zhora1337 <balakirev.zhora@yandex.ru>
2019-06-08 15:17:59 +0300

    Main page

--- RegulIT/urls.py
+++ RegulIT/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^main/', include('main.urls')),
+    url(r'^$', include('main.urls')),
 ]
