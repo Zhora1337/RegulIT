19ab7e09b47df67587e2f78f4f5a740b39d187da
Ubuntu <ubuntu@ip-172-31-21-94.us-east-2.compute.internal>
2019-06-09 12:38:33 +0000

    v

--- RegulIT/settings.py
+++ RegulIT/settings.py
@@ -118,10 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT =("RegulIT/main/static")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "main/static/"),
-]
+STATIC_URL = 'RegulIT/static/'
