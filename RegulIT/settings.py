1f3dc38e7ffc4c037ddb9bd0e09eae3a40fd8d0a
AlexeySevalnikov <51491806+AlexeySevalnikov@users.noreply.github.com>
2019-06-21 15:45:04 +0300

    added logo and name

--- RegulIT/settings.py
+++ RegulIT/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static/'
+#STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 
 STATICFILES_DIR = 'static/'
