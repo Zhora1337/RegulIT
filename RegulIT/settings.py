d04d86cf8c4d4657bb8f516a7aced17a9adccee9
Zhora1337 <balakirev.zhora@yandex.ru>
2019-07-02 16:32:12 +0300

    revert postgresql

--- RegulIT/settings.py
+++ RegulIT/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'RegulIT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'regul',
-	'USER': 'zhora1555',
-	'PASSWORD': 'fallen1998',
-	'HOST': '0.0.0.0',
-	'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
