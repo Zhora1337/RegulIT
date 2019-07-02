84840f784d395f40c6ce3e99a137795073124cd4
Zhora1337 <balakirev.zhora@yandex.ru>
2019-07-02 17:32:28 +0300

    path to sign up

--- RegulIT/urls.py
+++ RegulIT/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.urls import path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('main.urls')),
-    url(r'^signup/', include('signup.urls')),
+    path('signup/', include('signup.urls')),
 ]
