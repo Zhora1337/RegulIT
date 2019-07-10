d63499e124ce036fdc53cd4d5c29f1b4fea98fe0
Zhora1337 <balakirev.zhora@yandex.ru>
2019-07-10 15:48:31 +0300

    added postgre

--- RegulIT/settings.py
+++ RegulIT/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'RegulIT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'regul_db',
+        'USER' : 'zhora',
+        'PASSWORD' : 'password',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
