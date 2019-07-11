94751b2a695c15f181e3ab744059f8ebb5954931
AlexeySevalnikov <ya.seva910@yandex.ru>
2019-07-11 14:57:07 +0300

    navbar

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^AboutUs/', views.AboutUs, name= 'AboutUs')
+    url(r'^AboutUs/', views.AboutUs, name= 'AboutUs'),
+    url(r'^ourgoal/', views.ourgoal, name= 'ourgoal'),
+    url(r'^ourteam/', views.ourteam, name= 'ourteam')   
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def AboutUs(request):
     return render(request, 'main/AboutUs.html')
+
+def ourgoal(request):
+    return render(request, 'main/ourgoal.html')
+
+def ourteam(request):
+    return render(request, 'main/ourteam.html')
