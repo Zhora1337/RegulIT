39dbc238f71250e7029d47d486b851df4e275e0f
AlexeySevalnikov <ya.seva910@yandex.ru>
2019-07-12 16:13:35 +0300

    automatic language switching depending on the computer language

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^AboutUs/', views.AboutUs, name= 'AboutUs'),
     url(r'^ourgoal/', views.ourgoal, name= 'ourgoal'),
-    url(r'^ourteam/', views.ourteam, name= 'ourteam')   
+    url(r'^ourteam/', views.ourteam, name= 'ourteam'), 
 ]
