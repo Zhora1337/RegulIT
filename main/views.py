d407b9a803d26110ebb7484b1fca0e3e817e6804
AlexeySevalnikov <ya.seva910@yandex.ru>
2019-07-16 17:48:21 +0300

    Switch

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
+    from django.utils import translation
+    # user_language = 'ru'
+    # translation.activate(user_language)
+    # request.session[translation.LANGUAGE_SESSION_KEY] = user_language
+
+    if translation.LANGUAGE_SESSION_KEY in request.session:
+        del request.session[translation.LANGUAGE_SESSION_KEY]
+
     return render(request, 'main/home.html')
 
 def AboutUs(request):
