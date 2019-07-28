a731674bb22ad1dec78a3e273addfc81aab5b35d
Zhora1337 <balakirev.zhora@yandex.ru>
2019-07-28 18:16:30 +0300

    У каждого пользователя свои фото, после обработки фото удаляются

--- freetry/views.py
+++ freetry/views.py
@@ -1,36 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PhotoForm, ShowForm
 from django.http import HttpResponseRedirect
 from django.forms.models import model_to_dict
 from .models import Photo
 from django.shortcuts import get_object_or_404
+import os
 
 
 
 def index(request):
     if request.method =='POST':
-        form = PhotoForm(request.POST, request.FILES)
+        form = PhotoForm(request.POST, request.FILES, request.user)
         if form.is_valid():
-            form.photo = form.cleaned_data['photo'] 
-            form.save()
-            photo = Photo.objects.latest('id')
+            form.photo = form.cleaned_data['photo']
+            photo = form.save(commit=False)
+            print(photo.photo)
+            photo.user = request.user
+            photo.save()
             photo.height = photo.img_stat()[0]
             photo.width = photo.img_stat()[1]
-            photo.save()
-            return HttpResponseRedirect('/freetry/results/')
+            form.save()
+            os.remove(photo.photo.path)
+            return redirect('result', id=photo.id)
     else:
         form = PhotoForm()
     return render(request, 'freetry/index.html',{'form':form})
 
 def result(request, id):
     photo = get_object_or_404(Photo, id=id)
-    photo.height = photo.img_stat()[0]
-    photo.width = photo.img_stat()[1]
-    stat = ShowForm(instance=photo)
-    return render(request, 'freetry/result.html',{'form':stat})
+    return render(request, 'freetry/result.html',{'form':photo})
 
 
 def show(request):
-    photos = Photo.objects.all()
-    return render(request, 'freetry/results.html',{'photos':photos})
+    users_photos = Photo.objects.filter(user=request.user)
+    return render(request, 'freetry/results.html',{'photos':users_photos})
 
--- result/models.py
+++ result/models.py
@@ -1,4 +1,3 @@
 from django.db import models
-from freetry.models import Photo
 
 # Create your models here.
