5cf947c064a38e6d62ede0cbe7034bed83b7b9a8
AlexeySevalnikov <ya.seva910@yandex.ru>
2019-07-30 14:43:41 +0300

    send mail

--- mail/views.py
+++ mail/views.py
@@ -3,6 +3,6 @@ from django.core.mail import send_mail
 
 def index(request):
 
-    send_mail('ваш промокод', '35285dh', 'balakirev.zhora@yandex.ru',['georgy-pecheritsa@yandex.ru'], fail_silently=False)
+    send_mail('ваш промокод', '35285dh', 'aregulov@yandex.ru',['georgy-pecheritsa@yandex.ru'], fail_silently=False)
 
     return render(request, 'mail/index.html')
