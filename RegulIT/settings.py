7aeac5f0480aa8981a4caad8654c919d4a5d13fd
Zhora1337 <balakirev.zhora@yandex.ru>
2019-07-31 15:44:44 +0300

    rest api

--- RegulIT/settings.py
+++ RegulIT/settings.py
@@ -35,15 +35,15 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Application definition
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_USERNAME_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = ("mandatory")
 
 
 
 INSTALLED_APPS = [
+    'rest_framework',
     'result',
     'mail',
     'myprofile',
