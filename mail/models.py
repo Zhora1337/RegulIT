9ca5bd2c57a8b1206401252f1f5cd61307acba33
AlexeySevalnikov <ya.seva910@yandex.ru>
2019-08-05 15:50:22 +0300

    Six digit promocode

--- mail/models.py
+++ mail/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 import uuid
 
-def random_promocode(string_length=10):
+def random_promocode(string_length=6):
     """Returns a random string of length string_length."""
     random = str(uuid.uuid4()) # Convert UUID format to a Python string.
     random = random.upper() # Make all characters uppercase.
