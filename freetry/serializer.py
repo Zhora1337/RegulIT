5f8314361249dfcf03244f60c3206a948a9dbd29
Zhora1337 <balakirev.zhora@yandex.ru>
2019-08-10 22:59:30 +0300

    photo id

--- freetry/serializer.py
+++ freetry/serializer.py
@@ -17,6 +17,7 @@ class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = (
+            'id',
             'photo',
             'width'
         )
